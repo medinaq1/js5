878fadd12dd25ae288ef5b5e8da1affccd6890e6
medinaq1 <amangulovamedina006@gmail.com>
2024-10-09 18:23:48 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+ // переменный маалымат сактоочу жай 
+// var  lat const 
+
+ // const num = '100'
+// const num2 = '500'
+
+// console.log( parseInt( num) + parseInt( mun2));
+
+
+// const arr = ['atym', 'Medina', 'curl']
+// console.log(arr);
+// console.log(arr[1], arr[0], arr[2]);
+// console.log(arr[0])
+// console.log(arr[2]);
+
+// const num = parseInt('100')
+// const num2 =parseInt('500')
+ 
+// console.log (num+num2)
+
+// const array = [1, 'string', true, {}, 100n, null,undefined, Symbol'sym']
+
+// console.log(array);
+
+// const obj = {
+//     name:"Jonn",
+//     age:30,
+//     occupation:"developer",
+// }
+// console.log(obj);
+
+// const temp = parseInt( prompt("какая сегодня погода?"))
+
+// if (temp >30){
+//     console.log('Жарко');
+// } else if(temp >20){
+//     console.log('Тепло ');
+// } else {
+//     console.log("Холодно");
+    
+// }
+
+const grade =parseInt(prompt('Биздин 12 ай бар '))
+
+switch(grade){
+    case 1:
+        console.log('декабрь январь февраль');
+        break;
+        case 4:
+            console.log('март апрель май ');
+            break;
+            case 9:
+                console.log('июнь июль августь');
+               break;
+               case 12: 
+               console.log('сентябрь октябрь ноябрь');
+               break;
+               
+               case 13:
+                console.log('мындай ай жок');
+                break;
+                default:
+               }
+    
